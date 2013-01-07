9c0f53b87012c6618e16f889d686b8922313fcee
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-08 07:04:04 +0900

    * gen_tags.sh: don't depend on another script.

--- gen_tags.sh
+++ gen_tags.sh
@@ -6,11 +6,18 @@
 
 # Description:
 
-script="/home/yagnesh/.emacs.d/el-get/ncl-mode/ncl-ctags-gen.sh"
+function tag_gen()
+{
+ctags-exuberant -e -a --verbose=yes  --langdef=ncl \
+    --langmap=ncl:.ncl --regex-ncl='/^[[:space:]]*function[[:space:]]+([a-zA-Z0-9_]+)[:blank:]*.*/\1/f,function/' \
+    --regex-ncl='/^[[:space:]]*procedure[[:space:]]+([a-zA-Z0-9_]+)[:blank:].*/\1/p,procedure/' `find ${1:-"."} -type f -name "*.ncl"`
+}
 
 ## delete previously generated one.
 rm TAGS
 
-$script .
-$script ~/local/ncl/lib/ncarg/nclscripts/
+tag_gen .
+tag_gen ~/local/ncl/lib/ncarg/nclscripts/
+tag_gen ~/nsc/ncl-lib/
+
 # gen_tags.sh ends here
