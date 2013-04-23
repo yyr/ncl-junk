678bfecb0ceaa97e2b14a8f5be43bdcdbc29f73e
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-23 17:46:24 +0900

    * gen_tags.sh: use ncl-mode gen-tags script

--- gen_tags.sh
+++ gen_tags.sh
@@ -5,19 +5,10 @@
 #
 
 # Description:
-
-function tag_gen()
-{
-ctags -e -a --verbose=yes  --langdef=ncl \
-    --langmap=ncl:.ncl --regex-ncl='/^[[:space:]]*function[[:space:]]+([a-zA-Z0-9_]+)[:blank:]*.*/\1/f,function/' \
-    --regex-ncl='/^[[:space:]]*procedure[[:space:]]+([a-zA-Z0-9_]+)[:blank:].*/\1/p,procedure/' `find ${1:-"."} -type f -name "*.ncl"`
-}
-
-## delete previously generated one.
 rm TAGS
 
-tag_gen .
-tag_gen ~/local/ncl/lib/ncarg/nclscripts/
-tag_gen ~/nsc/ncl-lib/
+gen-tags.sh $(pwd)
+gen-tags.sh $NCARG/lib/ncarg/nclscripts/
+gen-tags.sh ~/nsc/ncl-lib/
 
 # gen_tags.sh ends here
