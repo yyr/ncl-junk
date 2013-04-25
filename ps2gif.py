ab4af3a12e92f1f052561ea26a911f31f55c0064
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-25 15:08:19 +0900

    * ps2gif.py: classify

--- ps2gif.py
+++ ps2gif.py
@@ -13,22 +13,51 @@ WEBSITE = "http://yagnesh.org"
 LICENSE ="GPL v3 or later"
 
 import re
-def ps_nopages(file_obj):
-    """Search header and return number of pages of given ps file contain.
-    """
-    for line in file_obj:
-        m = re.match("%%Pages: (\d+)",line)
-        if m:
-            return(m.group(1))
+import os.path
 
-    return None
+class psFile(object):
+    def __init__(self, fname):
+        """Manipulate psFile
+        Arguments:
+        - `fname`: file name of ps file
+        """
+        self._fname = fname
+        self._file_obj = open(self._fname,'r')
+        self._no_pages = self.no_of_pages(self._file_obj)
+
+
+    def no_of_pages(self,file_obj=None):
+        """Search header and return number of pages of given ps file contain.
+        """
+        if file_obj is None:
+            file_obj=self._file_obj
+
+        for line in file_obj:
+            m = re.match("%%Pages: (\d+)",line)
+            if m:
+                return(int(m.group(1)))
+
+        return None
+
+    def split2pages(self,fname=None,page_len=None):
+        if fname is None:
+            fname=self._fname
+        if page_len is None:
+            page_len = self._no_pages
+
+        import subprocess
+        fn, e = os.path.splitext(fname)
+        for i in range(page_len):
+           print('psselect -p%s %s > %s-%s.ps' % (i,fname,fn,e))
+            # subprocess.Popen('psselect -p' + i  )
+
 
 def arg_parse(file_name=None):
-    import os.path
     for fn in file_name:
         fPath, f = os.path.split(fn)
         fName, fExn = os.path.splitext(f)
-        print(f + " " + ps_nopages(open(f,'r')))
+        psf = psFile(f)
+        psf.split2pages()
 
 def main(args=None):
     import argparse
